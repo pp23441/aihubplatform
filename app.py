f155156a2a94f3410f7fcb753a400d2d4f3a4d33
Pruthvi Pandya <147524351+pp23441@users.noreply.github.com>
2026-02-06 11:15:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import timedelta
 from dotenv import load_dotenv
